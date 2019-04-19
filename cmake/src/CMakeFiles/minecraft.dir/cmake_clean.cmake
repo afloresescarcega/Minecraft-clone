8a36c2da65b8071fba7a4441dfe544b755d20997
afloresescarcega <alex.floresescar@gmail.com>
2019-04-19 13:12:47 -0500

    add cmake

---
+++ cmake/src/CMakeFiles/minecraft.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minecraft.dir/gui.cc.o"
+  "CMakeFiles/minecraft.dir/main.cc.o"
+  "CMakeFiles/minecraft.dir/procedure_geometry.cc.o"
+  "CMakeFiles/minecraft.dir/render_pass.cc.o"
+  "CMakeFiles/minecraft.dir/shader_uniform.cc.o"
+  "../bin/minecraft.pdb"
+  "../bin/minecraft"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minecraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
